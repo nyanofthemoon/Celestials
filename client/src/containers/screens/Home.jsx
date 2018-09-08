0bfd055b9874da06430190575fc7f35bc0f2fa4b
Francois Lachance <latch2112@gmail.com>
2018-09-08 18:13:28 -0400

    Homescreen initial commit

--- client/src/containers/screens/Home.jsx
+++ client/src/containers/screens/Home.jsx
@@ -1,16 +1,33 @@
 import React, {Component, PropTypes} from 'react'
 
-import LoginForm from './../../components/forms/Login';
-import {noop} from './../../actions';
+import Button from '@material-ui/core/Button';
+import Drawer from '@material-ui/core/Drawer';
+
+
 
 
 class HomeScreen extends Component {
+    state = {
+        top: false,
+        left: false,
+        bottom: false,
+        right: false,
+    };
+
+
     render() {
         const {engine} = this.props;
-        return (<div className="flex-vertical-container">
-            <h1 className="logo">Home Screen</h1>
-            <LoginForm handleSubmit={noop}/>
-        </div>);
+        return (<div>
+                    <h1>Celestials</h1>
+                    <img src="http://fpoimg.com/300x300?text=Celestials Logo"/>
+                    <br/>
+                    <Button variant="contained">join</Button>
+                    <span>or</span>
+                    <Button variant="contained" color="primary">play</Button>
+
+
+                </div>
+        );
     }
 }
 

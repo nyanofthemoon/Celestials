6b225e2a77c0edb0487cb3d77becd10f79ebbde3
Nyan Of The Moon <nyanofthemoon@gmail.com>
2018-10-10 16:05:24 -0400

    joi examples

--- server/validation.js
+++ server/validation.js
@@ -1,3 +1,7 @@
+// https://github.com/hapijs/joi
+// Joi.validate(LOGIN_FORM_DATA, validate.auth.schema.body)
+// Joi.validate(JOIN_FORM_DATA, validate.account.schema.body)
+
 'use strict';
 
 const Joi = require('joi');
@@ -17,6 +21,8 @@ const validate = {
         password: Joi.string().required().min(5).max(32)
       }
   }},
+
+
 
 
 
